f2eb33d5f4f075824d8ea6671669db45c65e334e
Dr Gareth Owen <drgowen@gmail.com>
2013-10-24 16:59:27 +0100

    initial

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 fsfinder
 ========
 
-File system finder for when partition absent / can't be trusted
+File system finder for when partition absent / can't be trusted.  
+
+python3 fsfinder.py diskorimagefile
+
+Requires Python 3.
+
+NOTE: all sizes in sectors
+
+Sample output:
+
+    ~/fsfinder# python3 fsfinder.py ~/lab2.img   
+    found boot sector at sector 0  
+    found NTFS size=32065 at sector 63  
+    found boot sector at sector 21053  
+    found NTFS size=32065 at sector 32128  
+	found boot sector at sector 32130  
+
---
+++ fsfinder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright Gareth Owen <drgowen@gmail.com>
+import struct
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: fsfinder.py imageordisk")
+    sys.exit(1)
+
+sector = 0
+with open(sys.argv[1], "rb") as f:
+    while True:
+        sec = f.read(512)
+        found = False
+        if len(sec) < 512:
+            break
+        # Boot sector
+        if sec[510] == 0x55 and sec[511] == 0xaa:
+            found = "boot sector"
+        #NTFS filesystem
+        if sec[3:7] == b"NTFS":
+            sizedt = sec[0x28:0x30] #BPB
+            sizent = struct.unpack("<Q", sizedt)
+            found = "NTFS size={}".format(sizent[0])
+
+        #Linux ext filesystem
+        if sec[0x38] == 0x53 and sec[0x39]==0xef: #linux
+            blockcnt=struct.unpack("<I", sec[4:8])[0]
+            blksizeraw=1024<<struct.unpack("<I",sec[0x18:0x18+4])[0]
+            found = "Linux totalblocks={} blksize={} size={}".format(blockcnt, blksizeraw, (blockcnt*blksizeraw)//(1024*1024))
+
+        #did we find something?
+        if found:
+            print ("found {} at sector {}".format(found,sector))
+
+        sector += 1
+
